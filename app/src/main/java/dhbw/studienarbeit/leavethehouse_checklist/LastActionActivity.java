b521c1792ff90342c44871721aec896b4f8805e7
linkna <link.nata@gmail.com>
2020-11-11 21:40:04 +0100

    optimized imports

--- app/src/main/java/dhbw/studienarbeit/leavethehouse_checklist/LastActionActivity.java
+++ app/src/main/java/dhbw/studienarbeit/leavethehouse_checklist/LastActionActivity.java
@@ -4,14 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
